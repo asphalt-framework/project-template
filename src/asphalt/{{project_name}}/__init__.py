1a461649868301993b2cbbe1a72c8b8fa20d88e3
Alex Grönholm <alex.gronholm@nextday.fi>
2024-12-15 17:48:59 +0200

    Removed unnecessary variables

--- src/asphalt/{{project_name}}/__init__.py
+++ src/asphalt/{{project_name}}/__init__.py
@@ -1,10 +1,8 @@
-from typing import Any
-
 from ._component import Component as Component
 
 # Re-export imports so they look like they live directly in this package
-key: str
-value: Any
-for key, value in list(locals().items()):
-    if getattr(value, "__module__", "").startswith(f"{__name__}."):
-        value.__module__ = __name__
+for __value in list(locals().values()):
+    if getattr(__value, "__module__", "").startswith(f"{__name__}."):
+        __value.__module__ = __name__
+
+del __value
